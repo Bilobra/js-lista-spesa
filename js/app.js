6ce9e5657aafa67402d133c55d7f5d1158a43621
Bilobra <bra.carrozzo@gmail.com>
2022-07-27 14:39:42 +0200

    add array

--- js/app.js
+++ js/app.js
@@ -1,2 +1,14 @@
 // CONSOLELOG PROVA
 console.log('lista della spesa')
+
+// creare array lista della spesa
+// stampare nella pagina usando ciclo for
+
+const listaSpesa = [
+    'zucchero di canna',
+    'carta igienica',
+    'ragù di soia',
+    'ammorbidente',
+    'salmone affumicato',
+    'burro'
+]
